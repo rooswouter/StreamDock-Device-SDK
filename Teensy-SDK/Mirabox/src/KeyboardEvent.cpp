6afa44bff392e33bb2acff46525f028fdc99dd1e
rooswouter <54571617+rooswouter@users.noreply.github.com>
2026-07-14 10:13:45 +0200

    moved Teensy-SDK to https://github.com/rooswouter/TeensyMirabox

--- Teensy-SDK/Mirabox/src/KeyboardEvent.cpp
+++
@@ -1,8 +0,0 @@
-#include "KeyboardEvent.h"
-
-KeyboardEvent::KeyboardEvent(const uint8_t *data, size_t length) {
-    if (length != 8) {
-        return;
-    }
-    memcpy(keycode, data, length);
-}
